ae82f3b720ed667e57dfea0cbcf9ad4363550f4c
Evert Jan Stamhuis <ej@fromej.nl>
2025-08-04 09:46:36 +0200

    Set default database url to azure postgres connection string

--- foodfetcher/foodfetcher/settings.py
+++ foodfetcher/foodfetcher/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'foodfetcher.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DATABASE_URL = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
+
 DATABASES = {
     'default': dj_database_url.config(
+        default=DATABASE_URL or 'sqlite:///:memory:',
         conn_max_age=600,
-        conn_health_checks=True,
-    ),
+        ssl_require=True if DATABASE_URL else False
+    )
 }
 
 
