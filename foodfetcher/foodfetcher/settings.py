2098524b275d62f8707000736f3f7b64d4f68d83
Evert Jan Stamhuis <ej@fromej.nl>
2025-08-04 13:58:59 +0200

    final app to deploy

--- foodfetcher/foodfetcher/settings.py
+++ foodfetcher/foodfetcher/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-8j+j&wcfz^m&y4p$t7&ygwgs_j^)z-0y_em618@)xw%#c8x(g1')
+SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- foodfetcher/foods/tasks.py
+++ foodfetcher/foods/tasks.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 from .models import FavoriteFoods
 
-OPENAI_MODEL = "gpt-4.1-nano"
+OPENAI_MODEL = "gpt-4.1"
 
 @background(schedule=0)
 def get_favorite_foods(amount: int = 100):
@@ -19,7 +19,7 @@ def get_favorite_foods(amount: int = 100):
 
     all_responses = []
 
-    for _ in range(amount):
+    for n in range(amount):
         chat_a = openai_client.responses.create(
             model=OPENAI_MODEL,
             input="You are in a conversation with a true food lover. Ask him for his top three favorite foods."
@@ -28,14 +28,14 @@ def get_favorite_foods(amount: int = 100):
         chat_b = openai_client.responses.create(
         model=OPENAI_MODEL,
         input=chat_a.output_text,
-        instructions="You are culinairy food lover that has a very diverse taste of foods. Randomly you feel like being vegan or vegitarian or a true meat lover.",
+        instructions="You are culinairy food lover that has a very diverse taste of foods. Randomly you feel like being fully vegan, vegitarian or a true meat lover.",
         temperature=1.3,
         )
 
         post_processing = openai_client.responses.parse(
             model=OPENAI_MODEL,
             input=chat_b.output_text,
-            instructions="Extract the three favorite foods. Based on the favorite food determine if the user is vegitarian/vegan",
+            instructions="Extract the three favorite foods and determine if these are all vegitarian/vegan. If one of the foods clearly has meat in it, determine it as non vegitarian.",
             text_format=FavoriteFood,
         )
 
@@ -44,6 +44,7 @@ def get_favorite_foods(amount: int = 100):
             chat_b.output_text, 
             post_processing.output_parsed
             ))
+        print(f"{n + 1}/{amount}")
 
     food_responses = [
         FavoriteFoods(
